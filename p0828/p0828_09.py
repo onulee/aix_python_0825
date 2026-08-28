432390647f5dd1ccbe961149877148411bbdf8d1
onulee@naver.com <onulee@naver.com>
2026-08-28 17:14:33 +0900

    for문

--- p0828/p0828_09.py
+++ p0828/p0828_09.py
@@ -1,8 +1,55 @@
-for i in range(10):
-    print("안녕")
+# for i in range(2,10):
+#     for j in range(1,10):
+#         print("{} X {} = {}".format(i,j,i*j))
 
-for _ in range(10):
-    print("안녕")
+
+# # 번호표 001
+# for i in range(0,10):
+#     for j in range(0,10):
+#         for k in range(0,10):
+#             print("{}{}{}".format(i,j,k))
+
+
+# for i in range(0,10):
+#     for j in range(0,10):
+#         for k in range(0,10):
+#             print(i,j,k)
+
+
+
+
+# for i in range(1,10):
+#     print(f"3 X {i} = {3*i}")
+
+# sum = 0
+# for i in range(1,11):
+#     sum = sum+i
+#     if sum>11:
+#         print("10보다 크기 바로앞일때 :",i-1) #i-15
+#         print("10초과전 시점 : ",sum-i)   #55  5050 500500 50005000 5000050000
+#         break
+
+# sum 100넘어가는 시점은 i가 얼마일때?
+
+
+
+
+
+
+
+# for i in range(3):
+#     no = i+1
+#     name = input("이름 입력 : ")
+#     kor = int(input("국어점수 입력 : "))
+#     print("{}\t{}\t{}".format(no,name,kor))
+
+
+
+# for i in range(10):
+#     print("안녕")
+
+# for _ in range(10):
+#     print("안녕")
 
 
 
