65278f3befcf075760cbde3ecbfd240164aeb4d2
onulee@naver.com <onulee@naver.com>
2026-08-28 17:57:52 +0900

    학생성적프로그램

--- p0828/p0828_10.py
+++ p0828/p0828_10.py
@@ -6,5 +6,15 @@
 
 
 # 구구단을 출력하시오
+stu = []
+for i in range(100):
+    no = i+1
+    name = input("이름입력 : ")
+    kor = int(input("국어점수 입력 : "))
+    stu.append([no,name,kor])
+
+for i in range(2):
+    print("{}\t{}\t{}".format(stu[i][0],stu[i][1],stu[i][2]))
+
 
 
