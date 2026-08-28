f60ef850f4c26a093c839bf82e5170aed24f33bd
onulee@naver.com <onulee@naver.com>
2026-08-28 16:03:44 +0900

    조건문,반복문

---
+++ p0828/p0828_09.py
@@ -0,0 +1,30 @@
+for i in range(10):
+    print("안녕")
+
+for _ in range(10):
+    print("안녕")
+
+
+
+# # for 변수 in 범위:
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,5):  
+#     print(i*10)  
+
+# for i in range(0,10,2):
+#     print(i)  
+
+
+# for i in [1,5,3,2]:
+#     print(i)  
+
+# for i in "안녕하세요":
+#     print(i) 
+
+# arr = list(range(1,11))          
+# print(arr)
+
+
+
