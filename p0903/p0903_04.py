4e544a6f95902a5d6a0f8a033d3184e44d93a33f
onulee@naver.com <onulee@naver.com>
2026-09-03 17:56:25 +0900

    쇼핑몰리스트

--- p0903/p0903_04.py
+++ p0903/p0903_04.py
@@ -11,6 +11,22 @@ product = [
     {"p_name":"오디오","price":500000,"bonusPoint":500000*0.1},
 ]
 
+def cal1(choice):
+    no = int(input(f"{product[choice-1]['p_name'] }를 구매하시겠습니까?(구매:1,취소:0) "))
+    if no == 1:
+        print(f"{product[choice-1]['p_name'] } 구매완료")
+        # 계산후 결과
+        my_info['money'] -= product[choice-1]['price']
+        # my_info['money'] = my_info['money'] - product[0]['price']
+
+        my_info['bonusPoint'] += product[choice-1]['bonusPoint']
+        print(f"m머니 : {my_info['money']:,}원")
+        print(f"m보너스포인트 : {my_info['bonusPoint']:,}포인트")
+    else:
+        print("이전화면으로 이동합니다.") 
+
+
+
 # 아이디,패스워드 확인
 while True:
     print("[ 쇼핑몰에 오신것을 환영합니다. ]")
@@ -42,32 +58,11 @@ while True:
 
     # 1.컴퓨터구매부분
     if choice == 1:
-        no = int(input(f"{product[choice-1]['p_name'] }를 구매하시겠습니까?(구매:1,취소:0) "))
-        if no == 1:
-            print(f"{product[choice-1]['p_name'] } 구매완료")
-            # 계산후 결과
-            my_info['money'] -= product[choice-1]['price']
-            # my_info['money'] = my_info['money'] - product[0]['price']
-
-            my_info['bonusPoint'] += product[choice-1]['bonusPoint']
-            print(f"m머니 : {my_info['money']:,}원")
-            print(f"m보너스포인트 : {my_info['bonusPoint']:,}포인트")
-        else:
-            print("이전화면으로 이동합니다.")    
+        cal1(choice)    
     elif choice == 2:
-        no = int(input("냉장고를 구매하시겠습니까?(구매:1,취소:0) "))
-        if no == 1:
-            print("냉장고 구매완료")
-            # 계산후 결과
-            my_info['money'] -= product[1]['price']
-
-            my_info['bonusPoint'] += product[1]['bonusPoint']
-            print(f"m머니 : {my_info['money']:,}원")
-            print(f"m보너스포인트 : {my_info['bonusPoint']:,}포인트")
-        else:
-            print("이전화면으로 이동합니다.") 
+        cal1(choice)
     elif choice == 3:
-        pass
+        cal1(choice)
 
 
 
