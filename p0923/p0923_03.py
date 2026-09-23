7615ea519ef84238adc4879d9b9cead9f39e1776
onulee@naver.com <onulee@naver.com>
2026-09-23 12:41:17 +0900

    네이버웹툰

---
+++ p0923/p0923_03.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
